ccc91f8dbacd362fb02b23e331633ce47dee056d
feng qin <qinfeng0152@gmail.com>
2014-11-26 11:13:52 +0100

    ajout location (geopoint) -> afterSave "Restaurant"

--- cloud/main.js
+++ cloud/main.js
@@ -54,6 +54,35 @@ Parse.Cloud.afterDelete("TodayDish", function(req){
     }
 )
 
+Parse.Cloud.afterSave("Restaurant",function(req){
+        var resto = req.object;
+        var address = resto.get("address");
+        var city = resto.get("city");
+        var postalCode = resto.get("postalCode");
+        var queryString = address+", "+postalCode+", "+city+", FR";
+
+        Parse.Cloud.httpRequest({
+            url: 'https://maps.googleapis.com/maps/api/geocode/json',
+            params: {
+                address : queryString,
+                key : 'AIzaSyA9odZ-917yCv0JZWxpNa8TYBRk7HDgg_w'
+            },
+            success: function(httpResponse) {
+                var geoLocation = httpResponse.data.results[0].geometry.location;
+                var point = new Parse.GeoPoint({latitude: geoLocation.lat, longitude: geoLocation.lng});
+                resto.set("location",point);
+                resto.save();
+                console.log("saved location");
+            },
+            error: function(httpResponse) {
+                console.error('Request failed with response code ' + httpResponse.status);
+            }
+        });
+
+})
+
+
+
 
 
 
